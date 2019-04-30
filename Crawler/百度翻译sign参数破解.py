ca4112df56b325a526012be1427448e79f2e7de8
ck <1668308657@qq.com>
2019-04-30 05:27:13 -0700

    通过计算sign的js函数来动态生成sign

---
+++ Crawler/百度翻译sign参数破解.py
@@ -0,0 +1,67 @@
+import requests
+import execjs
+
+
+url = 'https://fanyi.baidu.com/v2transapi'#接口url
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+    'Cookie': 'BAIDUID=461E83774C0B37E576C71B4540FB5D4E:FG=1; BIDUPSID=461E83774C0B37E576C71B4540FB5D4E; PSTM=1541078623; BDORZ=B490B5EBF6F3CD402E515D22BCDA1598; to_lang_often=%5B%7B%22value%22%3A%22en%22%2C%22text%22%3A%22%u82F1%u8BED%22%7D%2C%7B%22value%22%3A%22zh%22%2C%22text%22%3A%22%u4E2D%u6587%22%7D%5D; REALTIME_TRANS_SWITCH=1; FANYI_WORD_SWITCH=1; HISTORY_SWITCH=1; SOUND_SPD_SWITCH=1; SOUND_PREFER_SWITCH=1; from_lang_often=%5B%7B%22value%22%3A%22zh%22%2C%22text%22%3A%22%u4E2D%u6587%22%7D%2C%7B%22value%22%3A%22en%22%2C%22text%22%3A%22%u82F1%u8BED%22%7D%5D; MCITY=-218%3A; delPer=0; PSINO=1; locale=zh; H_PS_PSSID=1458_28777_21078_28768_28720_28964_28831_28584_28604; Hm_lvt_64ecd82404c51e03dc91cb9e8c025574=1555660396,1556594444,1556622552,1556622647; Hm_lpvt_64ecd82404c51e03dc91cb9e8c025574=1556622647'
+}
+inputdata = input('请输入要查询的：')
+#打开js代码原文，通过使用pyExecJS来调用js函数动态生成sign
+with open('baidu.js')as f:
+    jsData = f.read()
+    p = execjs.compile(jsData).call('e',inputdata)
+form_data = {
+    'from':'en',
+    'to':'zh',
+    'query':inputdata,
+    'transtype'	:'realtime',
+    'simple_means_flag':'3',
+    'sign':p,
+    'token':'89a9aae25e5801524b3577516650cfcf'
+}
+
+r = requests.post(url = url,headers = headers,data=form_data)
+r.encoding = 'utf-8'
+with open('baidufanyi.html','w',encoding='utf-8')as fp:
+    fp.write(r.text)
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+'''
+# # # # # # # js代码原文，另起一文件# # # # # # # # # # # # # 
+
+var i = "320305.131321201"
+
+function n(r, o) {
+    for (var t = 0; t < o.length - 2; t += 3) {
+        var a = o.charAt(t + 2);
+        a = a >= "a" ? a.charCodeAt(0) - 87 : Number(a), a = "+" === o.charAt(t + 1) ? r >>> a : r << a, r = "+" === o.charAt(t) ? r + a & 4294967295 : r ^ a
+    }
+    return r
+}
+
+
+function e(r) {
+    var o = r.match(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g);
+    if (null === o) {
+        var t = r.length;
+        t > 30 && (r = "" + r.substr(0, 10) + r.substr(Math.floor(t / 2) - 5, 10) + r.substr(-10, 10))
+    } else {
+        for (var e = r.split(/[\uD800-\uDBFF][\uDC00-\uDFFF]/), C = 0, h = e.length, f = []; h > C; C++) "" !== e[C] && f.push.apply(f, a(e[C].split(""))), C !== h - 1 && f.push(o[C]);
+        var g = f.length;
+        g > 30 && (r = f.slice(0, 10).join("") + f.slice(Math.floor(g / 2) - 5, Math.floor(g / 2) + 5).join("") + f.slice(-10).join(""))
+    }
+    var u = void 0, l = "" + String.fromCharCode(103) + String.fromCharCode(116) + String.fromCharCode(107);
+    u = null !== i ? i : (i = window[l] || "") || "";
+    for (var d = u.split("."), m = Number(d[0]) || 0, s = Number(d[1]) || 0, S = [], c = 0, v = 0; v < r.length; v++) {
+        var A = r.charCodeAt(v);
+        128 > A ? S[c++] = A : (2048 > A ? S[c++] = A >> 6 | 192 : (55296 === (64512 & A) && v + 1 < r.length && 56320 === (64512 & r.charCodeAt(v + 1)) ? (A = 65536 + ((1023 & A) << 10) + (1023 & r.charCodeAt(++v)), S[c++] = A >> 18 | 240, S[c++] = A >> 12 & 63 | 128) : S[c++] = A >> 12 | 224, S[c++] = A >> 6 & 63 | 128), S[c++] = 63 & A | 128)
+    }
+    for (var p = m, F = "" + String.fromCharCode(43) + String.fromCharCode(45) + String.fromCharCode(97) + ("" + String.fromCharCode(94) + String.fromCharCode(43) + String.fromCharCode(54)), D = "" + String.fromCharCode(43) + String.fromCharCode(45) + String.fromCharCode(51) + ("" + String.fromCharCode(94) + String.fromCharCode(43) + String.fromCharCode(98)) + ("" + String.fromCharCode(43) + String.fromCharCode(45) + String.fromCharCode(102)), b = 0; b < S.length; b++) p += S[b], p = n(p, F);
+    return p = n(p, D), p ^= s, 0 > p && (p = (2147483647 & p) + 2147483648), p %= 1e6, p.toString() + "." + (p ^ m)
+}
+'''
